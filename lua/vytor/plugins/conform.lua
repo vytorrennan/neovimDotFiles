774ab0758e3f3620219a5ac6ddf4b925d90cc346
vytorrennan <crosdeadbr@gmail.com>
2024-02-10 18:17:04 -0300

    Adicionando suporte a formater clang e comentario para ver nome de formatters

--- lua/vytor/plugins/conform.lua
+++ lua/vytor/plugins/conform.lua
@@ -3,10 +3,12 @@ return {
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
+                -- :help conform-formatters
 				lua = { "stylua" },
 				go = { "goimports", "gofmt" },
 				javascript = { { "prettierd", "prettier" } },
-				python = { "black" }
+				python = { "black" },
+                java = { "clang-format" }
 			},
 		})
 	end,
